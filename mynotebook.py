0d451234db3b223970c02765ff7b49a313b12525
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2020-12-29 18:16:58 +0500

    Update mynotebook.py

--- mynotebook.py
+++ mynotebook.py
@@ -2,22 +2,23 @@
 # autor Bulat
 
 import tkinter
-import json
 
 root = tkinter.Tk()
 root.title("mynotebook")
 root.geometry("350x180")
 dicer = dict()
 def clicked():
-    dicer[ent_eng.get()] = ent_ru.get()
-    x = ent_eng.get()
+    key = ent_eng.get()
+    val = ent_ru.get()
+    dicer[key] = val
     lbl3.configure(text = ent_eng.get())
     lbl4.configure(text = ent_ru.get())
-    with open('engru.json', 'a') as file:
-        json.dump(dicer, file, ensure_ascii=False)
-        del dicer[x]
-        ent_eng.delete(0, "end")
-        ent_ru.delete(0, "end")
+    with open('enru.txt', 'a') as file:
+        for key, val in dicer.items():
+            file.write("{}:{}\n".format(key,val))
+            del dicer[key]
+            key.delete(0, "end")
+            val.delete(0, "end")
 lbl1 = tkinter.Label(root, text = "english")
 lbl2 = tkinter.Label(root, text = "russian")
 lbl3 = tkinter.Label(root, text = "")
