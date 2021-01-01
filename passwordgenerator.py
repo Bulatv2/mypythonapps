603dd785204871f68d58834d8c736843c9888bef
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-01-01 12:18:57 +0500

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,12 +1,30 @@
 import random
-
+import json
 print("password generator and save")
 s = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 p = ""
-l = int(input("length: "))
+dicw = {}
+def main():
+    ffi()
+def save():
+    n = input("web-site: ")
+    dicw[n] = p
+    with open("passwords.json", "a") as file:
+        json.dump(dicw, file)
+        del dicw[n]
+        print("save!")
+def ffi():
+    l = int(input("password length: "))
+    generate(l)
 def generate(x):
     global p
     for i in range(x):
         p += random.choice(s)
-generate(l)
-print("password is {}".format(p))
+    print("password is {}".format(p))
+    n = input("save password (y/n)? ")
+    if n == "y":
+        save()
+    elif n == "n":
+        main()
+while True:
+    main()
