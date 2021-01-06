b383b3abe07eb69f8d0dd51069f88023ecdf3b99
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-01-06 16:00:01 +0500

    Update mytaskmanager.py

--- mytaskmanager.py
+++ mytaskmanager.py
@@ -1,62 +1,34 @@
 # mytaskmanager
 # author Bulat
 
-import json
 print("mytaskmanager \n")
 print(" a - add task \n s - show task \n t - take task \n d - done task \n h - help\n")
 tasklist = []
-progresslist = []
-donelist = []
 def main():
-    x = input("command ")
-    if x == "a":
+    cd = input("command ")
+    if cd == "a":
         add()
-    elif x == "s":
+    elif cd == "s":
         show()
-    elif x == "t":
+    elif cd == "t":
         take()
-    elif x == "d":
-        done()
-    elif x == "h":
+    elif cd == "h":
         help()
-    elif x == "q":
+    elif cd == "q":
         quit()
 def add():
-    y = input("task: ")
-    save(y)
-    tsave(y)
-def save(x):
-    tasklist.append(x)
+    task = input("task: ")
+    save(task)
+def save(task):
+    tasklist.append(task)
 def take():
-    progresslist.append(tasklist[0])
+    print("{} is done.".format(tasklist[0]))
     del tasklist[0]
-    print("ok")
-def done():
-    donelist.append(progresslist[0])
-    del progresslist[0]
-    print("{} is done".format(donelist[0]))
 def show():
-    if len(tasklist) == 0 and len(donelist) == 0:
-        with open("tasks.txt", "r") as file:
-            for line in file:
-                print(line)
-    else:
-        print("to do:")
-        for i in range(len(tasklist)):
-            print(tasklist[i])
-        if len(progresslist) != 0:
-            print("doing:")
-            for i in range(len(progresslist)):
-                print(progresslist[i])
-                if len(donelist) != 0:
-                    print("done:")
-                    for i in range(len(donelist)):
-                        print(donelist[i]) 
+    print("to do:")
+    for i in range(len(tasklist)):
+        print(tasklist[i])
 def help():
     print("this is taskmanager.")
-def tsave(x):
-    with open("tasks.txt", "a") as file:
-        file.write("{}\n".format(x))
 while True:
     main()
-
