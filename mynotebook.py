dbf99ef557ec147f3285e2df23bdcaa2d0b58e1d
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-01-22 22:41:16 +0500

    Update mynotebook.py

--- mynotebook.py
+++ mynotebook.py
@@ -5,21 +5,31 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("mynotebook")
-root.geometry("350x180")
-dicer = {}
+root.geometry("450x200")
+forcount = {}
+with open("enru.txt", "r") as file:
+    for row in file:
+        if not row:
+            continue
+        else:
+            eng, rus = row.split(":")
+            forcount[eng] = rus
+count = len(forcount.items())
 def save():
-    dicer[ent_eng.get()] = ent_ru.get()
+    global count
     lbl3.configure(text = ent_eng.get())
     lbl4.configure(text = ent_ru.get())
-    with open('enru.txt', 'a') as file:
+    count += 1
+    lbl5.configure(text = count)
+    with open("enru.txt", "a") as file:
         file.write("{}:{}\n".format(ent_eng.get(),ent_ru.get()))
-        del dicer[ent_eng.get()]
         ent_eng.delete(0, "end")
         ent_ru.delete(0, "end")
 lbl1 = tkinter.Label(root, text = "english")
 lbl2 = tkinter.Label(root, text = "russian")
 lbl3 = tkinter.Label(root, text = "")
 lbl4 = tkinter.Label(root, text = "")
+lbl5 = tkinter.Label(root, text = "")
 ent_eng = tkinter.Entry(root, width = 40)
 ent_eng.focus()
 ent_ru = tkinter.Entry(root, width = 40)
@@ -31,4 +41,5 @@ ent_ru.pack()
 btn1.pack()
 lbl3.pack()
 lbl4.pack()
+lbl5.pack()
 root.mainloop()
