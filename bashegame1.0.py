efa4467d56eaf9f7b8562d24ef2617bb9a862c30
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2021-01-24 15:54:46 +0500

    Update bashegame1.0.py

--- bashegame1.0.py
+++ bashegame1.0.py
@@ -1,68 +1,61 @@
-# Bashe Game v1.0
+# Bashe Game
 # author Bulat
 
 import random
 
 print('Game Bashe. There are 11 items. Rivals go in turn, for each move \nany of the players can take 1, 2 or 3 items. \nLoses the one who is forced to take the last item.')
 print('Игра Баше. 11 предметов. Соперники ходят по очереди, за каждый ход \nлюбой  из играющих может взять 1, 2 или 3 предмета. \nПроигрывает тот, кто вынужден \nвзять последний предмет.')
-k = 11
-# 11 things. The game can have 7, 11, 15, 19, 21, ... things.
-c = 0
-# how much things should take, how much take and for win algorithm for app
-u = 0
-a = 0
-# u - user last take flag == 1, a - app last take flag == 1
-while k > 1:
-    userwr = int(input('Enter: number 1, 2 or 3: '))
-    k -= userwr
-    u = 1
-    print('Total left: ', k)
-    if k >= 1:
-        # first step to know how to get win algorithm for app
-        if a != 1 and userwr == 1:
-            c = 4 - userwr
-            # how to count for app
-            c = 1
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif a != 1 and userwr == 2:
-            c = random.randint(1, 3) 
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif a != 1 and userwr == 3:
-            c = 3
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif k >= 4:
-            c = 4 - userwr
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif k == 3:
-            c = 2
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif k == 2:
-            c = 1
-            k -= c
-            # app's step
-            a = 1
-            u = 0
-        elif k == 1:
-            continue
-    print('The app took: ', c)
-    print('Total left: ', k)
-print()
-if k == 1 and u == 1:
-    print('You won!')
-elif k == 1 and a == 1:
-    print('You lost!')
+
+things = 11 # 11 things. The game can have 7, 11, 15, 19, 21, ... things.
+first = 0
+
+def main():
+    global things
+    user = int(input('Enter: number 1, 2 or 3: '))
+    things -= user
+    logic(user)
+def printr(take):
+    global things
+    global first
+    if things == 0:
+        print("You win!")
+        things = 11
+        first = 0
+    elif things == 1:
+        print("You lost! Total left {}".format(things))
+        things = 11
+        first = 0
+    else:
+        print("The app took {}. Total left {}".format(take, things))
+def logic(usertake):
+    global things
+    global first
+    if things > 1:
+        if first == 0:
+            if usertake == 1:
+                first = 1
+                take = 1
+                things -= take
+                printr(take)
+            elif usertake == 2:
+                first = 1
+                take = random.randint(1, 3)
+                things -= take
+                printr(take)
+            elif usertake == 3:
+                first = 1
+                take = 3
+                things -= take
+                printr(take)
+        else:
+           take = winalgorithm(usertake)
+           things -= take
+           printr(take)
+    elif things == 1:
+        take = things - 1
+        printr(take)
+def winalgorithm(usertake):
+    take = 4 - usertake
+    return take
+while True:
+    main()
